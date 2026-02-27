be9fa7c59068260db17c5facd61534427436b8a4
danny <dansalvey@icloud.com>
2026-02-27 14:05:02 -0500

    Add files via upload

---
+++ danny.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function buttonToggle(id) {
+    var x = document.getElementById(id);
+
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
